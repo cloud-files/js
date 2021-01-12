06cded0160dbf4bd56985ae2be0ee359d294e86a
cloud-files <63906823+cloud-files@users.noreply.github.com>
2021-01-12 12:05:59 +0800

    Add files via upload

---
+++ cjg.js
@@ -0,0 +1,64 @@
+//顶部广告
+document.write('<style>@media only screen and (min-width:0px) and (max-width:980px){ .max30h { max-height:28px !important; } .max170h { max-height:170px !important; }}</style>');
+$('.vodbody').before('<div class="topad wr">'+
+'<a href="http://45.145.230.183:2020/vip782.html" rel="external nofollow" target="_blank"><img class="max30h" src="http://pic.cytcm.com/2019260055/1050-80.gif"  style="max-height:80px;"></a>'+
+'<a href="https://x20008.com?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="https://5551722.com/452094222bb8448596bf5b67d27a2a39.gif"></a>'+
+'<a href="https://x20008.com?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="https://5551722.com/452094222bb8448596bf5b67d27a2a39.gif"></a>'+
+'<a href="https://x20008.com?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="https://5551722.com/452094222bb8448596bf5b67d27a2a39.gif"></a>'+
+'<a href="https://v63399.com?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="https://3337785.com/214402b0b3454f61ba25b5a0992b4b96.gif"></a>'+
+'<a href="https://v63399.com?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="https://3337785.com/214402b0b3454f61ba25b5a0992b4b96.gif"></a>'+
+'<a href="https://v63399.com?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="https://3337785.com/214402b0b3454f61ba25b5a0992b4b96.gif"></a>'+
+'<a href="https://s53999.com/?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="https://3337739.com/f020398a8b114ac7885bb84039896acb.gif"></a>'+
+'<a href="https://s53999.com/?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="https://3337739.com/f020398a8b114ac7885bb84039896acb.gif"></a>'+
+'<a href="https://s53999.com/?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="https://3337739.com/f020398a8b114ac7885bb84039896acb.gif"></a>'+
+'<a href="https://k7166.com?register=1" rel="external nofollow" target="_blank"><img src="https://3337723.com/469a0f8098bb406ab6aba0487f4aaecd.gif"></a>'+
+'<a href="https://k7166.com?register=1" rel="external nofollow" target="_blank"><img src="https://3337723.com/469a0f8098bb406ab6aba0487f4aaecd.gif"></a>'+
+'<a href="https://k7166.com?register=1" rel="external nofollow" target="_blank"><img src="https://3337723.com/469a0f8098bb406ab6aba0487f4aaecd.gif"></a>'+
+'<a href="http://2207139.com/#/?register=1" rel="external nofollow" target="_blank"><img src="http://www.1990tu.com/i/20210106171845svi.gif"></a>'+
+'<a href="http://2207139.com/#/?register=1" rel="external nofollow" target="_blank"><img src="http://www.1990tu.com/i/20210106171845svi.gif"></a>'+
+'<a href="http://8824955.com/#/?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="http://www.1990tu.com/i/20210111000417quj.gif"></a>'+
+'<a href="http://8824955.com/#/?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="http://www.1990tu.com/i/20210111000417quj.gif"></a>'+
+'<a href="http://8824955.com/#/?register=1" rel="external nofollow" target="_blank"><img class="max30h" src="http://www.1990tu.com/i/20210111000417quj.gif"></a>'+
+'<a href="https://x3x310.com/?proxyAccount=10055" rel="external nofollow" target="_blank"><img src="http://www.1990tu.com/i/2021011001014032r.gif"></a>'+
+'<a href="https://x3x310.com/?proxyAccount=10055" rel="external nofollow" target="_blank"><img src="http://www.1990tu.com/i/2021011001014032r.gif"></a>'+
+'<a href="https://x3x310.com/?proxyAccount=10055" rel="external nofollow" target="_blank"><img src="http://www.1990tu.com/i/2021011001014032r.gif"></a>'+
+'<a href="https://7610117.cc/" rel="external nofollow" target="_blank"><img src="http://www.1990tu.com/i/20201231170341nh7.gif"></a>'+
+'<a href="https://7610117.cc/" rel="external nofollow" target="_blank"><img src="http://www.1990tu.com/i/20201231170341nh7.gif"></a>'+
+'<a href="https://7610117.cc/" rel="external nofollow" target="_blank"><img src="http://www.1990tu.com/i/20201231170341nh7.gif"></a>'+
+'<a href="https://c4tt7.com" rel="external nofollow" target="_blank"><img src="https://cdn.jsdelivr.net/gh/cloud-files/imgs/tiantian-160.gif"></a>'+
+'<a href="http://103.15.106.51:7777/66070592.html" rel="external nofollow" target="_blank"><img src="https://cdn.jsdelivr.net/gh/cloud-files/imgs/xiaoxingxing9-1003.gif"></a>'+
+'<a href="https://131qq10.com" rel="external nofollow" target="_blank"><img src="https://cdn.jsdelivr.net/gh/cloud-files/imgs/alityc-88-3.gif"></a>'+
+'<a href="https://aa8h6.com" rel="external nofollow" target="_blank"><img src="https://cdn.jsdelivr.net/gh/cloud-files/imgs/AA8-160.gif"></a>'+
+'<a href="https://vv56a.com" rel="external nofollow" target="_blank"><img src="https://cdn.jsdelivr.net/gh/cloud-files/imgs/hy-wns-1.gif"></a>'+
+'<a href="https://xx6epj.com" rel="external nofollow" target="_blank"><img src="https://cdn.jsdelivr.net/gh/cloud-files/imgs/hy-xpj-1.gif"></a>'+
+'<a href="https://y82yl.com" rel="external nofollow" target="_blank"><img src="https://cdn.jsdelivr.net/gh/cloud-files/imgs/hy-ylhg-1.gif"></a>'+
+'<a href="https://hjd56.com" rel="external nofollow" target="_blank"><img  src="https://cdn.jsdelivr.net/gh/cloud-files/img/huangjin-2.gif"></a>'+
+'<a href="https://hjd56.com" rel="external nofollow" target="_blank"><img  src="https://cdn.jsdelivr.net/gh/cloud-files/img/huangjin-2.gif"></a>'+
+'<a href="http://hhtzff.com/?xyg168" rel="external nofollow" target="_blank"><img class="max30h" src="https://cbu01.alicdn.com/img/ibank/2018/273/940/8576049372_872274545.jpg"></a>'+
+'<a href="http://hhtzff.com/?xyg168" rel="external nofollow" target="_blank"><img class="max30h" src="https://cbu01.alicdn.com/img/ibank/2018/273/940/8576049372_872274545.jpg"></a>'+
+'<a href="http://hhtzff.com/?xyg168" rel="external nofollow" target="_blank"><img class="max30h" src="https://cbu01.alicdn.com/img/ibank/2018/273/940/8576049372_872274545.jpg"></a>'+
+'<a href="http://2227785.com/#/?register=1" rel="external nofollow" target="_blank"><img  class="max170h" src="https://3337750.com/34b801eada884981951807711195e1fc.gif"></a>'+
+'<a href="http://1706878.com/?register=1" rel="external nofollow" target="_blank"><img src="https://3337739.com/aa6143ec6a954ebdb44bdf43066fa4a2.gif"></a>'+
+'<a href="http://1706878.com/?register=1" rel="external nofollow" target="_blank"><img src="https://3337739.com/aa6143ec6a954ebdb44bdf43066fa4a2.gif"></a>'+
+'<a href="http://www.hndida.com/?k48" rel="external nofollow" target="_blank"><img class="max30h" src="https://alime-pic.oss-cn-hangzhou.aliyuncs.com/file/1597500702761/960x85.gif"></a>'+
+'<a href="http://www.hndida.com/?k48" rel="external nofollow" target="_blank"><img class="max30h" src="https://alime-pic.oss-cn-hangzhou.aliyuncs.com/file/1597500702761/960x85.gif"></a>'+
+'<a href="http://www.hndida.com/?k48" rel="external nofollow" target="_blank"><img class="max30h" src="https://alime-pic.oss-cn-hangzhou.aliyuncs.com/file/1597500702761/960x85.gif"></a>'+
+'<a href="https://7595527.com/#/?register=1" rel="external nofollow" target="_blank"><img  src="https://3337783.com/71c5670eba9c45668288363c32afcb1e.gif"></a>'+
+'<a href="https://7595527.com/#/?register=1" rel="external nofollow" target="_blank"><img  src="https://3337783.com/71c5670eba9c45668288363c32afcb1e.gif"></a>'+
+'<a href="https://1035507.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://qfihdr.com/9a96b937d358492e804d3b36354d0b82.gif"></a>'+
+'<a href="https://1035507.com/#/?register=1" rel="external nofollow" target="_blank"><img src="https://qfihdr.com/9a96b937d358492e804d3b36354d0b82.gif"></a>'+
+'</div>');
+
+//播放器广告
+$('.col-2-3').after('<div class="topad wr"></div>');
+
+if(location.href.indexOf('play')>0)
+{
+	  var r = Math.random();
+	  var n = Math.round(r);
+	  if (n == 0) {
+		document.write('<script src="https://cq.qdkzjdsb.cn/d/0A1DE1A0-E43A-4CCF-970E-BEF0F7062237.ap"><\/script>');
+	  } else {
+		document.write('<script src="https://xn--2qux23c9ziyrq.plus:4443/ty/x-3128-33.js"><\/script>');
+	}
+}
